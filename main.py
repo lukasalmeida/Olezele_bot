1721169abba62f05559ac7e161eec1d31a784980
Lucas Almeida <lalukasalmeida@gmail.com>
2026-05-13 17:36:17 -0300

    rerun

--- main.py
+++ main.py
@@ -1,41 +1,18 @@
 import discord
+from discord import emoji
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import asyncio
 
-from flask import Flask
-from threading import Thread
-
 from utils.logger import log
 from views.fechar_ticket_view import FecharTicketView
+
 from views.ticket_view import TicketView
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-# =========================
-# FLASK PARA RENDER
-# =========================
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot online"
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-
-def keep_alive():
-    t = Thread(target=run_web)
-    t.start()
-
-# =========================
-# DISCORD BOT
-# =========================
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -45,13 +22,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-
     bot.add_view(TicketView())
     bot.add_view(FecharTicketView())
 
     synced = await bot.tree.sync()
-
     print(f"Sincronizados: {len(synced)} comandos")
+
+
     print(f"Bot online como {bot.user}")
 
     embed = discord.Embed(
@@ -61,13 +38,10 @@ async def on_ready():
         timestamp=discord.utils.utcnow(),
     )
 
-    guild = bot.guilds[0]
-
-    await log(bot, guild.id, embed=embed)
+    await log(bot,guild.id, embed=embed)
 
 
 async def main():
-
     async with bot:
 
         await bot.load_extension("cogs.administrativo")
@@ -78,8 +52,5 @@ async def main():
         await bot.start(TOKEN)
 
 
-# inicia flask
-keep_alive()
 
-# inicia bot
 asyncio.run(main())
